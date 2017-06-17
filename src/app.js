0c03203e247695d4544220dbe03be72d74d0ad60
André Castelo <andrecastelo@gmail.com>
2017-06-17 00:55:50 -0300

    added rendering of react components

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
 console.log('Hello!');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Counter from './Counter.jsx';
+
+ReactDOM.render(
+    React.createElement(Counter),
+    document.getElementById('mount')
+);

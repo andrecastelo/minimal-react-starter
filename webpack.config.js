f90bec599c8f38d0a518ba3448d1bf5eea6c924c
André Castelo <andrecastelo@gmail.com>
2017-06-17 00:44:57 -0300

    changed the output dir to www

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     context: path.join(__dirname, 'src'),
     entry: [ './app.js' ],
     output: {
-        path: path.join(__dirname, 'dist'),
+        path: path.join(__dirname, 'www'),
         filename: 'bundle.js',
     },
     module: {
